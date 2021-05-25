ac8689e935113f335e779079577dc676ecc3d58a
mathew-success <mathew@success.app>
2021-05-25 17:10:00 +0530

    Removed the unwanted code in task model

--- app/Model/Tasks.php
+++ app/Model/Tasks.php
@@ -7,10 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Tasks extends Model
 {
     protected $fillable = ['title','description','due_date','assigned_to','project_id','created_at','updated_at'];
-
-	/*public function task(){
-      return $this->belongsTo('App\Model\Tasks','project_id');
-    }*/
+    
     public function project(){
       return $this->belongsTo('App\Model\Project');
     }
